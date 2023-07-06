447d2744a15250abf2b4eeb915f10d905d8e45ac
Christoph <31371749+Christoph-beep@users.noreply.github.com>
2023-07-06 21:39:40 +0200

    Add files via upload
    Try to append values into the map via a funcion

---
+++ Quiz/README.md
@@ -0,0 +1,9 @@
+# Quiz
+
+This is a litle Quiz game in Go, the answers to the program can be added in the main
+
+To do
+
+1. Fix the writing part, that answers can be written in the txt file -> done
+2. change the solutions in the answerFile into a map -> currently you can type in a wrong answer , but it may be right because it is the answer to another question
+3. write an admin login for adding questions
---
+++ Quiz/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// todo
+
+// 2. Creating file and directory for answering the answers
+// done
+// 3. writing answers to file with JSON
+
+// 4. compare userinput with solution -> therefore use toLowerCase() for the userAnswers
+
+// 5. Website stuff (handlers + forms for user input + get the user input in the backend  with formValue)
+// done
+
+func main() {
+	fmt.Println("Starting Server on port 8080")
+
+	//mux.HandleFunc("/quiz", quiz)
+	//mux.HandleFunc("/", quiz)
+
+	createAnswerDirectory()
+	createAnswerFile()
+	answerFileWrite("blau", 0)
+	answerFileWrite("hund", 1)
+
+	http.HandleFunc("/", quiz)
+	http.HandleFunc("/process", process)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+// Part 1
+
+func process(w http.ResponseWriter, r *http.Request) {
+
+	answersContent, err := os.ReadFile("answerDirectory/answers.txt")
+	if err != nil {
+		fmt.Println("error occured within the process function", err)
+	}
+	answersContentString := strings.ToLower(string(answersContent))
+
+	Question0 := r.FormValue("0")
+	Question1 := r.FormValue("1")
+
+	fmt.Println(Question0)
+	fmt.Println(Question1)
+
+	// check if answers were right
+	rightAnswersCounter := 0
+
+	fmt.Println(answersContentString + "inhalt file")
+	if strings.Contains(answersContentString, Question0) {
+		rightAnswersCounter++
+		fmt.Println("right answer 1")
+	} else {
+		fmt.Println("wrong 1")
+	}
+	if strings.Contains(answersContentString, Question1) {
+		rightAnswersCounter++
+		fmt.Println("right answer 2")
+	} else {
+		fmt.Println("wrong 2")
+	}
+
+	processedData, err := template.ParseFiles("process.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	processedData.Execute(w, rightAnswersCounter)
+
+}
+
+func quiz(w http.ResponseWriter, r *http.Request) {
+
+	// global variables
+
+	quizpage, err := template.ParseFiles("quiz.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	quizpage.Execute(w, nil)
+
+}
+
+// create Folder for the file with answers
+// if true, everything is right
+func createAnswerDirectory() bool {
+	_, err := os.Stat("answerDirectory")
+	if err != nil {
+		fmt.Println("Directory does not exist so far createAnswerDirectory")
+		// directory is created
+		os.MkdirAll("answerDirectory", os.ModePerm)
+		fmt.Println("directory has been created")
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+
+	}
+	fmt.Println("Directory already exists")
+	return true
+
+}
+
+// Creating File with answers
+// if true, everything is right
+func createAnswerFile() bool {
+	_, err := os.Stat("answerDirectory/answers.txt")
+	if err != nil {
+		fmt.Println("File does not exist so far")
+		// file is created
+		answerDirectory, err := os.Create("answerDirectory/answers.txt")
+		fmt.Println("file has been created successfully")
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+		defer answerDirectory.Close()
+
+	}
+	fmt.Println("everything is working properly (-:)")
+	return true
+
+}
+
+// Writing solutions to file -> can only be done by admins, needs to be implimented
+func answerFileWrite(newSolutions string, counter int) {
+	// reading part
+	// check, if data has already been written to the file, therefore the file needs to be opened first
+
+	// map for writing solutions into file
+	// new
+	// structure of maps : key[int], value[string]
+
+	// just a dummy counter not working !!!
+
+	type SolutionsQuiz struct {
+		SolutionsMap map[int]string `json:"solutions"`
+	}
+
+	solutions := SolutionsQuiz{
+		SolutionsMap: map[int]string{
+			counter: newSolutions,
+		},
+	}
+	fmt.Println("test")
+	fmt.Println(solutions)
+
+	jsonData, err := json.Marshal(solutions)
+	fmt.Println(jsonData)
+	var path = "answerDirectory/answers.txt"
+
+	fi, err := os.Stat("answerDirectory/answers.txt")
+	if err != nil {
+		fmt.Println("error occured, answerFileWrite", err)
+	}
+	// get the size
+	size := fi.Size()
+
+	if size == 0 {
+		// get number of curent elements within the map
+		err2 := os.WriteFile(path, []byte(jsonData), 0644)
+		if err2 != nil {
+			fmt.Println("error occured in answerFileWrite")
+			fmt.Println(err2)
+			return
+		}
+		fmt.Println("data has been successfuly written into file")
+	} else {
+		// text needs to be appended to prevent previous text from beeing deleted
+		f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Println(err)
+		}
+		defer f.Close()
+
+		_, err = f.Write([]byte(append(jsonData)))
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	fmt.Println("data has been successfuly appended to the eixsting file")
+}
